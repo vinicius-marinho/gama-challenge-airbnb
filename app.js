59b59b7f988c2ad6abbffd3ceb50010750238389
vinicius.marinho <vinicius.marinho@g.globo>
2020-05-08 21:51:00 -0300

    Estilização dos cards e adição do javascript

--- app.js
+++ app.js
@@ -2,9 +2,44 @@ const form = document.querySelector('form');
 const checkin = document.querySelector('form input[name="checkin"]')
 const checkout = document.querySelector('form input[name="checkout"]')
 
-// console.log(checkin)
+
+function preventWrongDate(){
+    if(checkin.value > checkout.value){
+        checkin.placeholder = 'Checkout menor que checkin'
+        checkin.value = ''
+        checkin.classList.toggle('date-failure')
+
+        checkout.placeholder = 'Checkout menor que checkin'
+        checkout.value = ''
+        checkout.classList.toggle('date-failure')
+        
+        
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(checkin.value)
+    preventWrongDate()
+    
 })
+
+fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72').then(response => {
+    if(!response.ok)
+        throw new Error(`Status Code Error: ${response.status}`)
+    return response.json()   
+
+}).then( data => {
+    constructCards(data);
+}).catch((data) => {
+    console.log(data)
+})
+
+function constructCards(data){
+    console.log(data[0])
+    data.map(value => {
+        console.log(value.photo)
+    })
+    // data.map(v => {
+    //     console.log(v[0])
+    // })
+}
