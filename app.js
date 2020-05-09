bc79c19a6f959736934039a2d54d9a5a97c34d37
vinicius.marinho <vinicius.marinho@g.globo>
2020-05-09 03:30:25 -0300

    Adicionando consulta a api e geração automática de cards

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('form');
 const checkin = document.querySelector('form input[name="checkin"]')
 const checkout = document.querySelector('form input[name="checkout"]')
+const main = document.querySelector('.main-wrapper')
+
 
 
 function preventWrongDate(){
@@ -35,9 +37,83 @@ fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72').then(respons
 })
 
 function constructCards(data){
-    console.log(data[0])
+
     data.map(value => {
-        console.log(value.photo)
+    let articles = document.createElement('article');
+    articles.classList.toggle('articles');
+
+    let articles_images = document.createElement('div');
+    articles_images.classList.toggle('articles-images');
+
+    let source_image = document.createElement('img');
+    source_image.src = value.photo
+
+    let tipo = document.createElement('div')
+    tipo.classList.toggle('tipo')
+
+    let tipo_texto = document.createElement('span');
+    tipo_texto.classList.toggle('tipo-texto')
+
+    let star = document.createElement('span');
+    star.classList.toggle('star');
+
+    let span_star = document.createElement('span');
+    span_star.style.color = "#FF385C"
+    span_star.innerText = "★"
+    
+
+    let span_rating = document.createElement('span');
+    span_rating.classList.toggle('rating')
+    span_rating.innerText = '5,00'
+
+    let body_height = document.createElement('div');
+    body_height.classList.toggle('body-height');
+
+    let p_body_height = document.createElement('p');
+    p_body_height.classList.toggle('body-article')
+    p_body_height.innerText = value.name
+
+    let footer_contents = document.createElement('div');
+    footer_contents.classList.add('text-footer');
+    footer_contents.classList.add('footer-left');
+
+    let preco = document.createElement('span');
+    preco.classList.toggle('preco');
+    preco.innerText = `R$${value.price}`
+
+    let diaria = document.createElement('span');
+    diaria.innerText = '/noite'
+
+    let valor_total = document.createElement('span');
+    valor_total.classList.toggle('valor-total')
+    valor_total.innerText = `Total de R$${value.price}`
+
+    main.appendChild(articles);
+    articles.appendChild(articles_images)
+    articles_images.appendChild(source_image)
+
+    articles.appendChild(tipo)
+    tipo_texto.innerText = value.property_type
+    tipo.appendChild(tipo_texto)
+    
+    tipo.appendChild(star)
+    star.appendChild(span_star)
+    star.appendChild(span_rating)
+
+    articles.appendChild(body_height)
+    body_height.appendChild(p_body_height)
+    body_height.appendChild(footer_contents)
+    footer_contents.appendChild(preco)
+    footer_contents.appendChild(diaria)
+
+    body_height.appendChild(valor_total)
+
+
+
+    })
+    
+    data.map(value => {
+        console.log(value)
     })
     // data.map(v => {
     //     console.log(v[0])
